ee469dd8c12215195e8ef8641ebfa9937031c94a
mathewtrivett <matt.trivett@gmail.com>
2018-12-24 11:15:37 +0000

    Beginning to get images working

--- themes/takecare/static/js/app.js
+++ themes/takecare/static/js/app.js
@@ -1,15 +1,21 @@
 
-function ImagePlacer() {
-  this.data = [];
+function ImagePlacer(data) {
+  this.data = data;
 }
 
-ImagePlacer.prototype.clickListen = function(tagname) {
+ImagePlacer.prototype.place = function(tagname) {
   let element = document.getElementsByTagName(tagname)[0];
   element.addEventListener('click', function(e) {
-    var coords = {
+    var imgObj = {
       "xPos": e.clientX,
-      "yPos": e.clientY
+      "yPos": e.clientY,
     }
-    this.data.push(coords)
   })
+  imgObj.img = this.randomChoice(this.data)
+  this.data.push(imgObj)
+}
+
+ImagePlacer.prototype.randomChoice = function(choices) {
+  let index = Math.floor(Math.random() * choices.length);
+  return choices[index];
 }
