2db3253ab0d555d62619dec380d73d5aeb9beba3
mathewtrivett <matt.trivett@gmail.com>
2018-12-25 16:15:18 +0000

    Changes to templates

--- themes/takecare/static/js/app.js
+++ themes/takecare/static/js/app.js
@@ -2,22 +2,24 @@
 
 var placedImages = [];
 var imageContainer = document.getElementById('images');
-var images = JSON.parse(imageContainer.dataset.images);
 
-imageContainer.onclick = function(event) {
-  let imgData = {
-    "xPos": event.clientX,
-    "yPos": event.clientY,
-    "src": randomChoice(images)
+if (imageContainer != undefined) {
+  var images = JSON.parse(imageContainer.dataset.images);
+  imageContainer.onclick = function(event) {
+    let imgData = {
+      "xPos": event.clientX,
+      "yPos": event.clientY,
+      "src": randomChoice(images)
+    }
+    placedImages.push(imgData);
+    var img = document.createElement('img');
+    img.src = imgData.src;
+    img.style.position = "absolute";
+    img.style.left = imgData.xPos + "px";
+    img.style.top = imgData.yPos + "px";
+    img.classList.add('placed');
+    this.appendChild(img);
   }
-  placedImages.push(imgData);
-  var img = document.createElement('img');
-  img.src = imgData.src;
-  img.style.position = "absolute";
-  img.style.left = imgData.xPos + "px";
-  img.style.top = imgData.yPos + "px";
-  img.classList.add('placed');
-  this.appendChild(img);
 }
 
 function randomChoice(choices) {
